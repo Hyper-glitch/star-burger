f3c916e106cd1e6708f121f9a29e67ac4014b7a5
hyper-glitch <graff049@gmail.com>
2022-08-23 11:04:40 +0300

    feature(validator): add module for validate data

--- foodcartapp/validators.py
+++ foodcartapp/validators.py
@@ -1,8 +1,10 @@
 import phonenumbers
-from rest_framework.response import Response
 
 
 def validate_products(raw_order):
+    product_key = 'products'
+    error_text = None
+
     try:
         products = raw_order['products']
     except KeyError as exc:
@@ -10,58 +12,55 @@ def validate_products(raw_order):
 
     match products:
         case str():
-            content = {'products': 'Ожидался list со значениями, но был получен str'}
+            error_text = 'Ожидался list со значениями, но был получен str'
         case None:
-            content = {'products': 'Это поле не может быть пустым'}
+            error_text = 'Это поле не может быть пустым'
         case []:
-            content = {'products': 'Этот список не может быть пустым'}
+            error_text = 'Этот список не может быть пустым'
         case KeyError():
-            content = {'products': 'Обязательное поле'}
-        case _:
-            serializer = OrderSerializer(data=raw_order)
-            serializer.is_valid(raise_exception=True)
-            serializer.save(products=products)
-            json = JSONRenderer().render(serializer.data)
-            return Response(json)
+            error_text = 'Обязательное поле'
+
+    content = {product_key: error_text}
+    return content if error_text else None
 
 
 def validate_order(raw_order: dict):
     copied_order = raw_order.copy()
     copied_order.pop('products')
     order_keys = ''
-    phonenumber = copied_order.get('phonenumber')
     error_text = None
-    content = None
-
-    validate_phone_number(phonenumber)
+    phonenumber = copied_order.get('phonenumber')
 
     if not copied_order:
         order_keys += 'firstname, lastname, phonenumber, address'
         error_text = 'Обязательное поле'
         return {order_keys: error_text}
 
+    content = validate_phone_number(phonenumber) if phonenumber else None
+    if content:
+        return content
+
     for idx, key_value in enumerate(copied_order.items()):
         key, value = key_value
         match value:
             case None | '':
                 error_text = 'Это поле не может быть пустым'
-                if 0 < idx < len(copied_order.keys()) - 1:
+                if 0 < idx:
                     order_keys += ', '
                 order_keys += f'{key}'
             case []:
                 error_text = 'Not a valid string'
-                if 0 < idx < len(copied_order.keys()) - 1:
+                if 0 < idx:
                     order_keys += ', '
                 order_keys += f'{key}'
 
-    content = {order_keys: error_text}
-    return content
+    content = {order_keys.lstrip(', '): error_text}
+    return content if error_text else None
 
 
-def validate_phone_number(phonenumber):
+def validate_phone_number(phonenumber: str) -> None | dict:
     parsed_phonenumber = phonenumbers.parse(phonenumber)
-    is_phone_valid = phonenumbers.is_valid_number(parsed_phonenumber)
-    if not is_phone_valid:
+    if not phonenumbers.is_valid_number(parsed_phonenumber):
         order_keys = 'phonenumber'
         error_text = 'Введен некорректный номер телефона'
         return {order_keys: error_text}
