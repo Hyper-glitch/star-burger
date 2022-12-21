52177a158f5d18c02716caa6153a73fab9594dbf
hyper-glitch <graff049@gmail.com>
2022-12-21 21:37:58 +0300

    feat(admin): move inlines

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -3,15 +3,10 @@ from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
 
-from .models import Product, Order, OrderItem
+from .inlines import OrderItemInline, RestaurantMenuItemInline
+from .models import Product, Order
 from .models import ProductCategory
 from .models import Restaurant
-from .models import RestaurantMenuItem
-
-
-class RestaurantMenuItemInline(admin.TabularInline):
-    model = RestaurantMenuItem
-    extra = 0
 
 
 @admin.register(Restaurant)
@@ -108,11 +103,6 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(ProductCategory)
 class ProductAdmin(admin.ModelAdmin):
     pass
-
-
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    extra = 0
 
 
 @admin.register(Order)
---
+++ foodcartapp/inlines.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import RestaurantMenuItem, OrderItem
+
+
+class RestaurantMenuItemInline(admin.TabularInline):
+    model = RestaurantMenuItem
+    extra = 0
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
