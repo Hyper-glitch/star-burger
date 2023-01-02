c6c80efe105bc209c2c997eed27ba98344781d78
hyper-glitch <graff049@gmail.com>
2023-01-02 11:40:59 +0300

    feat(migration): add payment field

--- foodcartapp/migrations/0044_order_payment.py
+++ foodcartapp/migrations/0044_order_payment.py
@@ -6,13 +6,19 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('foodcartapp', '0043_auto_20221226_0734'),
+        ("foodcartapp", "0043_auto_20221226_0734"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='order',
-            name='payment',
-            field=models.CharField(choices=[('CH', 'НАЛИЧНЫМИ'), ('OE', 'ОНЛАЙН')], db_index=True, default='CH', max_length=2, verbose_name='способ оплаты'),
+            model_name="order",
+            name="payment",
+            field=models.CharField(
+                choices=[("CH", "НАЛИЧНЫМИ"), ("OE", "ОНЛАЙН")],
+                db_index=True,
+                default="CH",
+                max_length=2,
+                verbose_name="способ оплаты",
+            ),
         ),
     ]
