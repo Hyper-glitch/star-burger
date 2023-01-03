0953cd3b6bad27dd7f363e7e033d5f44873872f9
hyper-glitch <graff049@gmail.com>
2023-01-03 15:33:50 +0300

    feat(utils): add calculate_order_distances

--- restaurateur/view_utils.py
+++ restaurateur/view_utils.py
@@ -1,4 +1,8 @@
 from typing import Any
+from django.db.models import QuerySet
+
+from foodcartapp.models import Order, Restaurant
+from geocoder.client import YandexGeocoderAPI
 
 
 def set_order_total_prices(orders, items):
@@ -18,8 +22,9 @@ def count_total_prices(items) -> dict[str, Any]:
     return pk_prices
 
 
-def intersect_order_restaurants(orders) -> None:
-    restaurants = {}
+def intersect_order_restaurants(client: YandexGeocoderAPI, orders: QuerySet) -> None:
+    rests = {}
+    rests_with_coords = {}
 
     for order in orders:
         for item in order.items.all():
@@ -27,13 +32,42 @@ def intersect_order_restaurants(orders) -> None:
             for menu_item in product.menu_items.all():
                 if menu_item.availability:
                     restaurant = menu_item.restaurant
+                    rests_with_coords.update(
+                        {
+                            restaurant.pk: client.fetch_coordinates(
+                                address=restaurant.address
+                            )
+                        }
+                    )
                 else:
                     continue
-                if not restaurants.get(product.pk):
-                    restaurants.update({product.pk: [restaurant]})
+                if not rests.get(product.pk):
+                    rests.update({product.pk: [restaurant]})
                 else:
-                    restaurants[product.pk].append(restaurant)
+                    rests[product.pk].append(restaurant)
 
-        values = list(restaurants.values())
-        order.restaurants = set.intersection(*map(set, values))
-        restaurants = {}
+        values = list(rests.values())
+        intersected_rests: set[Restaurant] = set.intersection(*map(set, values))
+        calculate_order_distances(
+            order=order,
+            client=client,
+            rests=intersected_rests,
+            rests_with_coords=rests_with_coords,
+        )
+        rests = {}
+
+
+def calculate_order_distances(
+    order: Order,
+    client: YandexGeocoderAPI,
+    rests: set[Restaurant],
+    rests_with_coords: dict[int, list[str]],
+) -> None:
+    order_coords = client.fetch_coordinates(address=order.address)
+    rests_distances = {}
+
+    for rest in rests:
+        rests_distances[rest] = client.calculate_distance(
+            order_coords=order_coords, rest_coords=rests_with_coords[rest.pk]
+        )
+    order.distances = rests_distances
